97134e5f0576f7a044866cf6a0c48e039e7ebdbf
Jonny Rimkus <jonny@rimkus.it>
2022-06-12 08:44:39 +0000

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd "$HOME" || exit 1
 source pg_venv/bin/activate
-gunicorn  -D \
-          --bind 0.0.0.0:8080 \
-          --workers=1 \
-          --threads=25 \
-          --chdir pg_venv/lib/python3.9/site-packages/pgadmin4 \
-          pgAdmin4:app
 
 gunicorn  -D \
           --bind 0.0.0.0:8081 \
@@ -14,3 +8,12 @@ gunicorn  -D \
           --threads=4 \
           --chdir pg_venv/lib/python3.9/site-packages/pgbackup \
           pgbackup:app
+
+gunicorn  --bind 0.0.0.0:8080 \
+          --workers=1 \
+          --threads=25 \
+          --chdir pg_venv/lib/python3.9/site-packages/pgadmin4 \
+          pgAdmin4:app
+
+#TODO: find a better solution to block,
+# sothat both gunicorn instances can run in background
